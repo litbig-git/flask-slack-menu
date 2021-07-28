73eb23320c82e577183d159d8ddd00cc1e55f3ae
Jacob Kang <jacob@litbig.com>
2021-07-28 20:31:28 +0900

    scheduler modify

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def post():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, port=5001)
--- scheduler.py
+++ scheduler.py
@@ -1,22 +1,22 @@
 import os
 import time
-
+import downloader
 import schedule
 
 
 def job():
-    os.system('python3 downloader.py month')
-    # print("I'm working...")
+	downloader.download_month()
+	print("job Done!")
 
 
 # 10초에 한번씩 실행
-# schedule.every(1).second.do(job)
+# schedule.every(10).seconds.do(job)
 # 10분에 한번씩 실행
 # schedule.every(10).minutes.do(job)
 # 매 시간 실행
 # schedule.every().hour.do(job)
-# 매일 08:30 에 실행
-schedule.every().day.at("08:30").do(job)
+# 매일 07:30 에 실행
+schedule.every().day.at("07:30").do(job)
 # 매주 월요일 실행
 # schedule.every().monday.do(job)
 # 매주 수요일 13:15 에 실행
